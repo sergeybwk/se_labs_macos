276363b93dc5ca14493c26221be47223beafac31
sergeybwk <sergeybwk@gmail.com>
2024-01-30 23:24:27 +0300

    Initial Commit

---
+++ se_labs_macos/se_labs_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  se_labs_macosApp.swift
+//  se_labs_macos
+//
+//  Created by Сергей Большаков on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct se_labs_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
