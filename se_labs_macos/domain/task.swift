4f6f2f4881587be0be84eaff3ef234f0e18340fa
aagolovanov <angol_02@mail.ru>
2024-02-07 02:58:11 +0300

    gavnina task

--- se_labs_macos/domain/task.swift
+++ se_labs_macos/domain/task.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+
+// task class
+class Task: TaskStateProtocol, CustomStringConvertible {
+    var IsDone: Bool = true
+    
+    var changeable: Bool = true
+
+    private(set) var description: String
+    
+    
+    init(description: String) {
+        self.description = description
+    }
+}
+
+protocol TaskStateProtocol {
+    var IsDone: Bool { get }
+}
