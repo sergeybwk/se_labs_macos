b7505eb3e31de068d3a6c5405db333c1cd996c0d
sergeybwk <sergeybwk@gmail.com>
2024-02-07 03:07:58 +0300

    taskview

---
+++ se_labs_macos/TaskView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskView.swift
+//  se_labs_macos
+//
+//  Created by Сергей Большаков on 07.02.2024.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskView()
+}
